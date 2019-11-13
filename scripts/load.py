dbb869e130e3184486ae1324ec29e42d33c2217d
Matt Baker <mbaker@shipt.com>
2019-11-13 12:30:39 -0600

    remove ipv4 check and do ipv6 load

--- scripts/load.py
+++ scripts/load.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from ipaddress import IPv4Network, IPv4Address
+from ipaddress import IPv4Network, IPv4Address, IPv6Network, IPv6Address
 from time import sleep
 from random import randint
 from random import getrandbits
@@ -62,6 +62,8 @@ requestTime = '"0.307" '
 upstreamResponseTime = '"0.296" '
 upstreamConnectTime = '"0.001"'
 subnets = ["24.0.0.0/8", "32.0.0.0/8"]
+ipv6 = ["2600:1700:9450:5db0:f9d1:be3e:d48c:743b", "2601:282:c02:63f0:581:2ab4:c407:da4e",
+        "2600:1700:e1c0:60f0:64f6:c41f:423d:36b8", "2601:2c4:4201:4cb0:38a7:ebae:4598:b5a6"]
 
 
 # --Writing the log file--
@@ -76,11 +78,12 @@ while True:
     # here, we combine the subnet and the random bits
     # to get an IP address from the previously specified subnet
     addr = IPv4Address(subnet.network_address + bits)
+    addr6 = random.choice(ipv6)
     addr_str = str(addr)
+    addrs = [addr_str, addr6]
     # have to add a space at the end for log formatting
     remoteAddr = addr_str + " "
-    httpXForwardedFor = "\"" + addr_str + "\" "
-    httpXForwardedForS = addr_str
+    httpXForwardedForS = addr6
 
     # --Logfile Generation--
     twohundreds = 100 - (fourErrs + fiveErrs)
